ee5f28bd8850d236eb52de23776699c23801687d
Agnelo David Fico Neto <a.david.fico.neto@avanade.com>
2021-02-23 13:04:11 -0300

    Alterações do firebase para site em construção

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import emconstrucao from './assets/emconstrucao.png';
+import logo from './assets/logo.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="Margin">OSEN <br /><span className='Subtitle'>Especialista em negócios</span></h1>
+        <h1 className="Margin"><img src={logo} alt="logo"/></h1>
         <h2 className="Margin">Fusão, Aquisição, Desenvolvimento de Negócios</h2>
-        <img src={emconstrucao} className="App-logo" alt="logo" />
-        <p>
-          19 99122.8252 |E-mail: os@osen.com.br <br/>
+        <img src={emconstrucao} className="App-logo" alt="Em construção" />
+        <p className="Paragraf">
+          Fone: <b>19 99122.8252</b> | E-mail: <b>os@osen.com.br</b><br/>
           End. Av. Washington Luiz, 1955 – Sala 02 – Vila Marieta – Campinas/ SP – CEP 13.142-105
         </p>
 
