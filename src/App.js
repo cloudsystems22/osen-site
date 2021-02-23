6dc7023024aa0ae2ab98407b68b3ff0dfdc3c45e
Agnelo David Fico Neto <a.david.fico.neto@avanade.com>
2021-02-23 08:45:00 -0300

    Primeira implantação

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import emconstrucao from './assets/emconstrucao.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="Margin">OSEN <br /><span className='Subtitle'>Especialista em negócios</span></h1>
+        <h2 className="Margin">Fusão, Aquisição, Desenvolvimento de Negócios</h2>
+        <img src={emconstrucao} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          19 99122.8252 |E-mail: os@osen.com.br <br/>
+          End. Av. Washington Luiz, 1955 – Sala 02 – Vila Marieta – Campinas/ SP – CEP 13.142-105
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
